bf712ff4b71c994dacbdfec924e10a264d70ec98
ShinySylveon04 <carlotta.basketball04@gmail.com>
2020-01-24 13:27:39 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,75 @@
+const Discord = require("discord.js");
+const {
+  prefix,
+  token,
+  helperRole,
+  roles,
+  okayEmoji,
+  giveawayChannel
+} = require("./config.json");
+
+const client = new Discord.Client();
+
+client.once("ready", () => {
+  console.log("Ready!");
+});
+
+const addGiveawayRole = message => {
+  const member = message.mentions.members.first();
+
+  if (member.id !== message.author.id) {
+    member
+      .addRole(helperRole)
+      .then(console.log(`Giveaway role added to ${member.displayName}`))
+      .then(message.react(message.guild.emojis.get(okayEmoji)))
+      .catch(console.error);
+  }
+};
+
+const roleCheck = message =>
+  message.member.roles.some(r => roles.includes(r.name));
+
+const removeRole = (message, membersWithRole) => {
+  if (membersWithRole.size !== 0) {
+    membersWithRole.map(member => {
+      member.removeRole(helperRole);
+    });
+
+    message.channel.send("Giveaway role removed!");
+  } else {
+    message.channel.send("No members have giveaway role!");
+  }
+};
+
+client.on("message", message => {
+  const membersWithRole = message.guild.roles.get(helperRole).members;
+
+  if (message.content.startsWith(prefix) && roleCheck(message)) {
+    const variable = message.content.split(prefix)[1];
+
+    if (variable === "ding") {
+      message.channel.send("Dong.");
+    }
+
+    if (message.channel.id === giveawayChannel) {
+      switch (variable) {
+        case "remove":
+          removeRole(message, membersWithRole);
+          break;
+        case "role":
+          message.channel.send(
+            `Got ${membersWithRole.size} members with giveaway role.`
+          );
+          break;
+        default:
+          break;
+      }
+    }
+  }
+
+  if (message.mentions.members && message.channel.id === giveawayChannel) {
+    addGiveawayRole(message);
+  }
+});
+
+client.login(token);
